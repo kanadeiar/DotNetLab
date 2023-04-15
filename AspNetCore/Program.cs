72f08130afee7e2a8bcac9ed735c8153fb62f4e3
kanadeiar <kanadeiar@gmail.com>
2023-04-15 10:17:48 +0300

    Добавление модуля

--- AspNetCore/Program.cs
+++ AspNetCore/Program.cs
@@ -1,21 +1,27 @@
+using AspNetCoreRazor.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AspNetCoreDbContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("AspNetCoreConnection"));
 });
-builder.WebHost.ConfigureServices(x => {
-    x.AddScoped<IStoreRepo, StoreRepo>();
-    x.AddScoped<IOrderRepo, OrderRepo>();
-    x.AddControllersWithViews();
-    x.AddRazorPages();
-    x.AddDistributedMemoryCache();
-    x.AddSession();
-    x.AddScoped<Cart>(x => SessionCart.GetCart(x));
-    x.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-    x.AddServerSideBlazor();
-    x.AddIdentity<IdentityUser, IdentityRole>()
-        .AddEntityFrameworkStores<AspNetCoreDbContext>();
-});
+builder.Services.AddScoped<IStoreRepo, StoreRepo>();
+builder.Services.AddScoped<IOrderRepo, OrderRepo>();
+var assembly = typeof(MyController).Assembly;
+builder.Services.AddControllersWithViews().AddApplicationPart(assembly).AddRazorRuntimeCompilation();
+builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(x =>
+    x.FileProviders.Add(new EmbeddedFileProvider(assembly)));
+
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(x => SessionCart.GetCart(x));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetCoreDbContext>();
 
 var app = builder.Build();
 
---
+++ AspNetCoreRazor/Controllers/MyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreRazor.Controllers;
+
+public class MyController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
