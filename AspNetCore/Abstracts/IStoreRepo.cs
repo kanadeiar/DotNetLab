ac0a10d08af24500721289454c5c925d92af2ecb
kanadeiar <kanadeiar@gmail.com>
2023-04-15 10:33:02 +0300

    Очистка решения

--- AspNetCore/Abstracts/IStoreRepo.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetCore.Abstracts;
-
-public interface IStoreRepo
-{
-    IQueryable<Product> Products { get; }
-    void CreateProduct(Product p);
-    void UpdateProduct(Product p);
-    void DeleteProduct(Product p);
-}
--- AspNetCore/ViewModels/ProductsListViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AspNetCore.ViewModels;
-
-public class ProductsListViewModel
-{
-    public IEnumerable<Product> Products { get; set; } = Array.Empty<Product>();
-    public PagingInfo? PagingInfo { get; set; }
-    public string? CurrentCategory { get; set; }
-}
