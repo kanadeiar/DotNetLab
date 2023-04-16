8d57fda86589f1eaea8ecfca3c95105d909fad7d
kanadeiar <kanadeiar@gmail.com>
2023-04-16 22:29:49 +0300

    Добавление пагинации по товароам

---
+++ KndStore.Catalog.Core/ViewModels/CatalogListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using KndStore.Catalog.Core.Entites;
+using KndStore.Shared.Core.ViewModels;
+
+namespace KndStore.Catalog.Core.ViewModels;
+
+public class CatalogListViewModel
+{
+    public IEnumerable<Product> Products { get; set; } = Array.Empty<Product>();
+    public PagingInfo? PagingInfo { get; set; }
+}
+
--- KndStore.Catalog/Controllers/CatalogController.cs
+++ KndStore.Catalog/Controllers/CatalogController.cs
@@ -1,4 +1,6 @@
 ﻿using KndStore.Catalog.Core.Abstracts;
+using KndStore.Catalog.Core.ViewModels;
+using KndStore.Shared.Core.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,15 +9,26 @@ namespace KndStore.Catalog.Controllers;
 public class CatalogController : Controller
 {
     private readonly ICatalogRepo _repo;
+    public int PageSize { get; set; } = 5;
     public CatalogController(ICatalogRepo repo)
     {
         _repo = repo;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(int productPage = 1)
     {
-        var products = await _repo.Query.ToArrayAsync();
-        return View(products);
+        var products = await _repo.Query.OrderBy(x => x.Id).Skip((productPage - 1) * PageSize).Take(PageSize).ToArrayAsync();
+        var model = new CatalogListViewModel
+        {
+            Products = products,
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalCount = _repo.Query.Count(),
+            },
+        };
+        return View(model);
     }
 }
 
